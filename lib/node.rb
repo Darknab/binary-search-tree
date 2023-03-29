54570c4672b05c4bd5bd4e0899273caa5c06dce0
Darknab <darknab@live.fr>
2023-03-29 12:35:22 +0100

    Moved the class definitions on seperate files

---
+++ lib/node.rb
@@ -0,0 +1,11 @@
+class Node
+    attr_accessor :left_child, :right_child, :parent, :value
+    
+    def initialize(value = nil, left_child = nil, right_child = nil, parent = nil)
+      @value = value
+      @left_child = left_child
+      @right_child = right_child
+      @parent = parent
+    end
+    
+  end
